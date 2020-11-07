5bd61c7f5918020d0a40c50853c757d51eec15fb
Joemn <Aruljoe37@gmail.com>
2020-11-07 23:44:08 +0530

    changed file structure  && added new files

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/Components/Projects/BirthdayCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BirthdayCard() {
+  return <div></div>;
+}
+
+export default BirthdayCard;
